d46c2bb70cf709b5dcd3ebef37744c400b9142dd
Surk Park <parksurk@gmail.com>
2014-02-07 17:13:28 +0000

    Updated web.js with displying from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var data = fs.readFileSync('index.html', 'utf8');
+console.log("index.html data : " + data);
+
+var buffer = new Buffer(fs.readFileSync('index.html', 'utf8'), 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
